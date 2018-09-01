7957b02020b4c372356e58839bd8ddaadd1a90a8
Paul Griffiths <mail@paulgriffiths.net>
2018-08-31 20:54:08 -0400

    Improve formatting.

--- graphs/internal/amGraph/am_graph.go
+++ graphs/internal/amGraph/am_graph.go
@@ -1,43 +1,43 @@
 package amGraph
 
 type amGraph struct {
-    matrix [][]byte
-    visited []bool
-    size int
+	matrix  [][]byte
+	visited []bool
+	size    int
 }
 
 func NewAmGraph(n int) *amGraph {
-    m := make([][]byte, n)
-    for i, _ := range m {
-        m[i] = make([]byte, n)
-    }
-    v := make([]bool, n)
-    return &amGraph{m, v, n}
+	m := make([][]byte, n)
+	for i, _ := range m {
+		m[i] = make([]byte, n)
+	}
+	v := make([]bool, n)
+	return &amGraph{m, v, n}
 }
 
 func (g *amGraph) InsertEdge(v1, v2 int) {
-    g.matrix[v1][v2] |= 1
-    g.matrix[v2][v1] |= 1
+	g.matrix[v1][v2] |= 1
+	g.matrix[v2][v1] |= 1
 }
 
 func (g *amGraph) Adjacent(v int) []int {
-    result := []int{}
-    for n := 0; n < len(g.matrix[v]); n++ {
-        if g.matrix[v][n] & 0x01 == 0x01 {
-            result = append(result, n)
-        }
-    }
-    return result
+	result := []int{}
+	for n := 0; n < len(g.matrix[v]); n++ {
+		if g.matrix[v][n]&0x01 == 0x01 {
+			result = append(result, n)
+		}
+	}
+	return result
 }
 
 func (g *amGraph) Visit(v int) {
-    g.visited[v] = true
+	g.visited[v] = true
 }
 
 func (g *amGraph) IsVisited(v int) bool {
-    return g.visited[v] == true
+	return g.visited[v] == true
 }
 
 func (g *amGraph) NumVertices() int {
-    return g.size
+	return g.size
 }
