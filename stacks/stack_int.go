7a8d79dd78c730cf3e7c631f041032aaf27f59da
Paul Griffiths <mail@paulgriffiths.net>
2018-08-31 20:26:32 -0400

    Fix bug in push method.

--- stacks/stack_int.go
+++ stacks/stack_int.go
@@ -3,35 +3,29 @@ package stacks
 // StackInt implements a stack of int elements.
 type StackInt struct {
 	elems []int
-	top   int
 }
 
 // NewStackInt creates a new stack of int elements.
 func NewStackInt() StackInt {
-	return StackInt{[]int{}, -1}
+	return StackInt{[]int{}}
 }
 
 // Push pushes a new int element onto the stack.
 func (s *StackInt) Push(n int) {
-	if s.top < len(s.elems) {
-		s.elems = append(s.elems, n)
-	} else {
-		s.elems[s.top+1] = n
-	}
-	s.top++
+	s.elems = append(s.elems, n)
 }
 
 // Pop pops the top int element from the stack.
 func (s *StackInt) Pop() int {
-	if s.top < 0 {
+	if len(s.elems) == 0 {
 		panic("stack underflow")
 	}
-	n := s.elems[s.top]
-	s.top--
+	n := s.elems[len(s.elems)-1]
+	s.elems = s.elems[:len(s.elems)-1]
 	return n
 }
 
 // IsEmpty returns true if the stack is empty, otherwise false.
 func (s *StackInt) IsEmpty() bool {
-	return s.top < 0
+	return len(s.elems) == 0
 }
