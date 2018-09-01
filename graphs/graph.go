4b336c930ac1504940b484f1db9e5b2be4bb1d19
Paul Griffiths <mail@paulgriffiths.net>
2018-08-31 20:26:52 -0400

    Implement iterative depth first search.

--- graphs/graph.go
+++ graphs/graph.go
@@ -2,6 +2,7 @@ package graphs
 
 import (
 	"github.com/paulgriffiths/gods/graphs/internal/amGraph"
+	"github.com/paulgriffiths/gods/stacks"
 )
 
 // Graph represents a graph type.
@@ -29,13 +30,40 @@ func DfsSpan(g Graph, v int) []int {
 	return l
 }
 
-// Dfs performs a depth first search of a graph.
+// DfsSpanIterative returns a depth first spanning tree for a graph.
+func DfsSpanIterative(g Graph, v int) []int {
+	l := []int{}
+	f := func(n int) {
+		l = append(l, n)
+	}
+	DfsIterative(g, v, f)
+	return l
+}
+
+// Dfs performs a recursive depth first search of a graph.
 func Dfs(g Graph, v int, f func(int)) {
 	g.Visit(v)
 	f(v)
 	for _, x := range g.Adjacent(v) {
 		if !g.IsVisited(x) {
 			Dfs(g, x, f)
+		}
+	}
+}
+
+// DfsIterative performs an iterative depth first search of a graph.
+func DfsIterative(g Graph, v int, f func(int)) {
+	s := stacks.NewStackInt()
+	s.Push(v)
+	for !s.IsEmpty() {
+		vtx := s.Pop()
+		if !g.IsVisited(vtx) {
+			g.Visit(vtx)
+			f(vtx)
+			a := g.Adjacent(vtx)
+			for i := len(a) - 1; i >= 0; i-- {
+				s.Push(a[i])
+			}
 		}
 	}
 }
--- graphs/graph_am_test.go
+++ graphs/graph_am_test.go
@@ -43,3 +43,21 @@ func TestDfs(t *testing.T) {
 		t.Errorf("got %v, want %v", l, []int{0, 1, 3, 7, 4, 5, 2, 6})
 	}
 }
+
+func TestDfsIter(t *testing.T) {
+	g := graphs.NewAMGraph(8)
+	g.InsertEdge(0, 1)
+	g.InsertEdge(0, 2)
+	g.InsertEdge(1, 3)
+	g.InsertEdge(1, 3)
+	g.InsertEdge(2, 5)
+	g.InsertEdge(2, 6)
+	g.InsertEdge(3, 7)
+	g.InsertEdge(4, 7)
+	g.InsertEdge(5, 7)
+	g.InsertEdge(6, 7)
+	l := graphs.DfsSpanIterative(g, 0)
+	if !IntSliceEqual(l, []int{0, 1, 3, 7, 4, 5, 2, 6}) {
+		t.Errorf("got %v, want %v", l, []int{0, 1, 3, 7, 4, 5, 2, 6})
+	}
+}
