e283d0563fb42ca9c21c3a04e780bf746b799369
Paul Griffiths <mail@paulgriffiths.net>
2018-09-02 23:08:58 -0400

    Handle invalid command line arguments.

--- cmd/maze/main.go
+++ cmd/maze/main.go
@@ -9,12 +9,33 @@ import (
 )
 
 func main() {
-	x := flag.Int("x", 25, "x dimension of maze")
-	y := flag.Int("y", 25, "y dimension of maze")
-	c := flag.Int("s", 30, "dimension of each maze cell in pixels")
+	x := flag.Int("x", 25, "x dimension of maze, >= 2")
+	y := flag.Int("y", 25, "y dimension of maze, >= 2")
+	c := flag.Int("s", 30, "dimension of each maze cell in pixels, >= 6")
 	f := flag.String("o", "stdout", "output file")
 	p := flag.Bool("p", false, "show path through maze")
 	flag.Parse()
+
+    argError := false
+    if *x < 2 {
+        fmt.Fprintf(os.Stderr, "%s: x dimension must be at least 2\n",
+            os.Args[0])
+        argError = true
+    }
+    if *y < 2 {
+        fmt.Fprintf(os.Stderr, "%s: y dimension must be at least 2\n",
+            os.Args[0])
+        argError = true
+    }
+    if *c < 6 {
+        fmt.Fprintf(os.Stderr, "%s: cell dimension must be at least 6\n",
+            os.Args[0])
+        argError = true
+    }
+
+    if argError {
+        os.Exit(1)
+    }
 
 	m := newMaze(*x, *y, *c)
 	g := m.toGraph()
